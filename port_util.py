ba30370d89e71f076a53b222a26ee970dc95b644
Adam Radford (aradford) <aradford@cisco.com>
2025-02-20 15:52:44 +1100

    Add code

---
+++ port_util.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+from argparse import ArgumentParser
+from dnacentersdk import api
+from dnacentersdk.exceptions import ApiError
+import logging
+import json
+from  time import sleep, time, strftime, localtime
+from dnac_config import DNAC, DNAC_USER, DNAC_PASSWORD
+import sys
+logger = logging.getLogger(__name__)
+timeout = 10
+
+def get_device_ports(dnac, deviceid,deviceip,hostname,snmp,platform):
+    portlist = dnac.devices.get_interface_info_by_id(deviceid)
+    for port in portlist.response:
+        # only get Ethernet ports
+        if port.portType == "Ethernet Port":
+            # skip trunks or routed ports
+            if port.portMode == "Trunk" or port.portMode == "routed":
+                continue
+            print(f'{deviceip},{hostname},{platform},{snmp},{port.serialNo},{port.portName},{port.portMode},{port.vlanId},{port.description},{port.lastIncomingPacketTime},{port.lastOutgoingPacketTime}')
+
+def main(dnac):
+    device_list = dnac.devices.get_device_list(family='Switches and Hubs')
+    device_attrs_list = [ (device.id,device.managementIpAddress,device.hostname,device.snmpLocation,device.platformId) for device in device_list.response]
+    # print the headers
+    print('DeviceIP,Hostname,Plaform,SNMPLocation,Serial,interface,PortMode,Vlan,Description,LastRx,LastTx')
+    for  device_attrs in device_attrs_list:
+        get_device_ports(dnac,*device_attrs)
+
+    #get_device_ports(dnac,*device_device_attrs[1])
+    #print (device_attrs_list)
+    
+if __name__ == "__main__":
+    parser = ArgumentParser(description='Select options.')
+    parser.add_argument('-v', action='store_true',
+                        help="verbose")
+    parser.add_argument('--password',  type=str, required=False,
+                        help='new passowrd')
+    parser.add_argument('--dnac',  type=str,default=DNAC,
+                        help='dnac IP')
+    args = parser.parse_args()
+
+    if args.v:
+        root_logger=logging.getLogger()
+        root_logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        ch = logging.StreamHandler()
+        ch.setFormatter(formatter)
+        root_logger.addHandler(ch)
+        logger.debug("logging enabled")
+
+    #logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+    DNAC = args.dnac
+    dnac = api.DNACenterAPI(base_url='https://{}:443'.format(DNAC),
+                                #username=DNAC_USER,password=DNAC_PASSWORD,verify=False,debug=True)
+                                username=DNAC_USER,password=DNAC_PASSWORD,verify=False)
+    main(dnac)
